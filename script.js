584e855a89159eff30bf17230cc46f7ab10a4800
Kelly Jones <kellymj6@gmail.com>
2021-08-24 16:00:07 -0400

    refactor planet response

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ window.addEventListener("load", function() {
     form.reset(); // needed to clear the form when the window is reloaded
     document.getElementById("faultyItems").style.visibility = "hidden"; // reset visibility of the form element
 
+    // get the form data...
+    form.addEventListener("submit", function(event) {
+        let pilotValue = document.querySelector("input[name=pilotName]").value;
+        let copilotValue = document.querySelector("input[name=copilotName").value;
+        let fuelLevelValue = document.querySelector("input[name=fuelLevel").value;
+        let cargoLevelValue = document.querySelector("input[name=cargoMass").value;
+        let list = document.getElementById("faultyItems");
+
+        // ...validate the form and update the launch status / list area of the page
+        formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue);
+        // ...but don't submit
+        event.preventDefault();
+    });
+
+
     let listedPlanets; // will hold array of objects
     
     // Set listedPlanetsResponse equal to the returned value of myFetch() 
@@ -14,7 +29,7 @@ window.addEventListener("load", function() {
         listedPlanets = result;
         console.log(listedPlanets);
  
-    }).then(function () {
+    //}).then(function () {
         console.log(listedPlanets);
     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
 
@@ -32,18 +47,6 @@ window.addEventListener("load", function() {
         // display the planet data for the mission target
         addDestinationInfo(document, planetName, planetDiameter, planetStar, planetDistance, planetMoons, planetImageUrl);
 
-        // get the form data...
-        form.addEventListener("submit", function(event) {
-            let pilotValue = document.querySelector("input[name=pilotName]").value;
-            let copilotValue = document.querySelector("input[name=copilotName").value;
-            let fuelLevelValue = document.querySelector("input[name=fuelLevel").value;
-            let cargoLevelValue = document.querySelector("input[name=cargoMass").value;
-            let list = document.getElementById("faultyItems");
-
-            // ...validate the form and update the launch status / list area of the page
-            formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue);
-            // ...but don't submit
-            event.preventDefault();
-        });
+    
     });
 });
